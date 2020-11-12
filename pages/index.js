35561b48bcfb67761a61deb61e762614ebe1980d
Clayton <clayton@moonshot.industries>
2020-11-12 11:06:36 -0500

    Removed SimpleTabs reference

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import SimpleTabs from '../components/SimpleTabs';
 import GamblingCard from '../components/GamblingCard';
 import IndexGrid from '../components/IndexGrid';
 
