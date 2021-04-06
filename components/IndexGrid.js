aecb5c757dd16cc73512a6a0feba4dd1974d2058
Clayton <cahanson12@gmail.com>
2021-04-06 07:34:01 -0400

    Update IndexGrid.js

--- components/IndexGrid.js
+++ components/IndexGrid.js
@@ -29,30 +29,28 @@ function IndexGrid() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper} elevation={3}>Updated 12/14/20<GamblingCard>
-          </GamblingCard>
+          <Paper className={classes.paper} elevation={3}>Updated 12/14/20<GamblingCard></GamblingCard>
           </Paper>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper} elevation={3}>Updated 12/12/20<CovidCard>
-          </CovidCard>
+          <Paper className={classes.paper} elevation={3}>Updated 12/12/20<CovidCard></CovidCard>
           </Paper>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper} elevation={3}>Posted 12/10/20<CompassRose>
-          </CompassRose></Paper>
+          <Paper className={classes.paper} elevation={3}>Posted 12/10/20<CompassRose></CompassRose>
+          </Paper>
         </Grid>
         <Grid item xs={12} sm={3}>
-          <Paper className={classes.paper} elevation={3}>Posted 12/2/20<ParisQ>
-          </ParisQ></Paper>
+          <Paper className={classes.paper} elevation={3}>Posted 12/2/20<ParisQ></ParisQ>
+          </Paper>
         </Grid>
         <Grid item xs={12} sm={5}>
           <Paper className={classes.paper} elevation={3}>Posted 11/18/20<MISunrise></MISunrise>
           </Paper>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper} elevation={3}>Posted 11/20/20<GamblingCard19>
-          </GamblingCard19></Paper>
+          <Paper className={classes.paper} elevation={3}>Posted 11/20/20<GamblingCard19></GamblingCard19>
+          </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
           <Paper className={classes.paper} elevation={3}>Posted 11/16/20<MIBoatRide></MIBoatRide>
