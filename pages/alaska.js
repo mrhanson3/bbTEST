e4955187e8dc09e33004f0a51eea97707565bdb7
Clayton <clayton@moonshot.industries>
2022-02-24 09:07:14 -0500

    Update alaska.js

--- pages/alaska.js
+++ pages/alaska.js
@@ -24,7 +24,7 @@ class Index extends React.Component {
         <p>Hi. We're putting together a list of the places that we're going to go and some other cool things.</p><p> This isn't set in stone, so if you have questions about a place, please speak up.</p>
         <Grid container spacing={2}>
           <Grid item xs={12} md={8}>
-            <Paper style={{ padding: '10px' }}><p><b>Itinerary</b></p>
+            <Paper style={{ padding: '10px' }}><p style={{ backgroundColor: '#f2f2f2', borderStyle: 'solid', padding: '10px', borderWidth: '1px', textAlign: 'center' }}><b>Itinerary</b></p>
             <p>Saturday, July 9 - Arrival day!</p>
 
             <p>Sunday, July 10 - Free morning, welcome party at Cathy's house starting mid-afternoon</p>
@@ -32,7 +32,7 @@ class Index extends React.Component {
             <p>Monday, July 11 - Trip to Girdwood to visit <a href="https://www.alaskawildlife.org/tours-programs/">Alaska Wildlife Conservation Center</a>,  <a href="https://girdwoodbrewing.com/welcome/">Girdwood Brewing</a>, stop at <a href="https://www.adfg.alaska.gov/index.cfm%3Fadfg%3Dviewinglocations.belugapoint">Beluga Point</a>, option to add on visit to <a href="https://www.alyeskaresort.com/">Alyeska Resort</a> for hiking/tram ride if open. </p>
 
             <p>Tuesday, July 12 - <a href="https://www.anchoragemuseum.org/">Anchorage museum </a>(with Discovery Center for kids).</p>
-            <p>Possible pool with water slide , possible dinner at <a href="https://www.humpysalaska.com/">Humpy's.</a></p>
+            <p>Possible pool with water slide, possible dinner at <a href="https://www.humpysalaska.com/">Humpy's.</a></p>
 
             <p>Wednesday, July 13 - EITHER trip to Eagle River and hike on <a href="https://www.tripadvisor.com/Attraction_Review-g30982-d4299711-Reviews-Mt_Baldy-Eagle_River_Anchorage_Alaska.html">Baldy</a> OR Visit <a href=" https://www.alaskanative.net/">Alaska Native Heritage Center</a>. Optional hike on <a href="https://www.alaska.org/detail/flattop-mountain">Flat Top.</a></p>
 
@@ -42,7 +42,7 @@ class Index extends React.Component {
             </Paper>
           </Grid>
           <Grid item xs={12} md={4}>
-            <Paper style={{ padding: '10px' }}><p><b>Places to Eat</b></p>
+            <Paper style={{ padding: '10px' }}><p style={{ backgroundColor: '#ffebe6', borderStyle: 'solid', padding: '10px', borderWidth: '1px', textAlign: 'center' }}><b>Places to Eat</b></p>
             <p><a href="https://www.snowcitycafe.com/">Snow City</a></p>
             <p><a href="https://www.humpysalaska.com/">Humpy’s</a></p>
             <p><a href="https://www.49statebrewing.com/">49th State Brewing</a></p>
@@ -51,17 +51,21 @@ class Index extends React.Component {
             </Paper>
           </Grid>
           <Grid item xs={12} md={6}>
-          <Paper style={{ padding: '10px' }}><p><b>Playgrounds</b></p>
-              <p><a href="https://www.alaska.org/detail/elderberry-park-anchorage">Elderberry Park (small but closest to downtown, only 0.2 miles from hotel) 1297 W 5th Avenue, Anchorage, AK</a></p>
-              <p><a href="https://www.alaska.org/detail/margaret-eagan-sullivan-park">Margaret Eagen Sullivan Park (1.3 miles from hotel, bigger playground)</a></p>
-              <p><a href="https://www.alaska.org/detail/valley-of-the-moon-park">Valley of the Moon Park (1.4 miles from hotel, good structure for bigger kids and separate one for younger)</a></p>
-              <p>Lyn Ary Park (Closer to Cathy’s house, on Tony Knowles Coastal Trail) 2009 Foraker Dr, Anchorage, AK 99517</p>
+          <Paper style={{ padding: '10px' }}><p style={{ backgroundColor: '#f7e6ff', borderStyle: 'solid', padding: '10px', borderWidth: '1px', textAlign: 'center' }}><b>Playgrounds</b></p>
+              <p><a href="https://www.alaska.org/detail/elderberry-park-anchorage">Elderberry Park</a> (small but closest to downtown, only 0.2 miles from hotel) 1297 W 5th Avenue, Anchorage, AK</p>
+              <p><a href="https://www.alaska.org/detail/margaret-eagan-sullivan-park">Margaret Eagen Sullivan Park</a> (1.3 miles from hotel, bigger playground)</p>
+              <p><a href="https://www.alaska.org/detail/valley-of-the-moon-park">Valley of the Moon Park</a> (1.4 miles from hotel, good structure for bigger kids and separate one for younger)</p>
+              <p><a href="https://www.alaska.org/detail/lynn-ary-park">Lyn Ary Park</a> (Closer to Cathy’s house, on Tony Knowles Coastal Trail) 2009 Foraker Dr, Anchorage, AK 99517</p>
           </Paper>
           </Grid>
           <Grid item xs={12} md={6}>
-          <Paper style={{ padding: '10px' }}><p><b>Things to Do</b></p>
+          <Paper style={{ padding: '10px' }}><p style={{ backgroundColor: '#ffffb3', borderStyle: 'solid', padding: '10px', borderWidth: '1px', textAlign: 'center' }}><b>Things to Do</b></p>
           <p>Walk/Bike <a href="https://anchoragecoastaltrail.com">Tony Knowles Coastal Trail</a></p>
           </Paper>
+          </Grid>
+          <Grid item xs={12} md={10}>
+          <p>See the little arrow button on the top left next to my pic, click it. Then you can see the names of the places.</p>
+          <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1A8-uJV7JH727WK-0mVbBZaMpZyaRsHoP&ehbc=2E312F" width="840" height="480"></iframe>
           </Grid>
         </Grid>
 
