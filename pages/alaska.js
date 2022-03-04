15acfdc2caca837a16e0a7984f392e8210400cc2
Clayton <clayton@moonshot.industries>
2022-03-04 09:13:01 -0500

    Update alaska.js

--- pages/alaska.js
+++ pages/alaska.js
@@ -21,7 +21,7 @@ class Index extends React.Component {
         </Link>
 
         <div>
-        <p>Hi. We're putting together a list of the places that we're going to go and some other cool things.</p><p> This isn't set in stone, so if you have questions about a place, please speak up.</p>
+        <p>Hi. We're putting together a list of the places that we're going to go and some other cool things. You're free to join as many of the group outings on the agenda as you want, or to do your own thing at any time!</p><p> This isn't set in stone, so if you have questions about a place, please speak up.</p>
         <Grid container spacing={2}>
           <Grid item xs={12} md={8}>
             <Paper style={{ padding: '10px' }}><p style={{ backgroundColor: '#f2f2f2', borderStyle: 'solid', padding: '10px', borderWidth: '1px', textAlign: 'center' }}><b>Itinerary</b></p>
@@ -29,7 +29,7 @@ class Index extends React.Component {
 
             <p>Sunday, July 10 - Free morning. Welcome party at Cathy's house starting mid-afternoon</p>
 
-            <p>Monday, July 11 - Road trip to Girdwood to visit <a href="https://www.alaskawildlife.org/tours-programs/">Alaska Wildlife Conservation Center</a>,  <a href="https://girdwoodbrewing.com/welcome/">Girdwood Brewing</a>, and have lunch. Stop at <a href="https://www.adfg.alaska.gov/index.cfm%3Fadfg%3Dviewinglocations.belugapoint">Beluga Point</a> for scenic vistas. All ages. Option to add on visit to <a href="https://www.alyeskaresort.com/">Alyeska Resort</a> for hiking/tram ride if open. </p>
+            <p>Monday, July 11 - Road trip to Girdwood to visit <a href="https://www.alaskawildlife.org/tours-programs/">Alaska Wildlife Conservation Center</a>,  <a href="https://girdwoodbrewing.com/welcome/">Girdwood Brewing</a>, and have lunch. Stop at <a href="https://www.adfg.alaska.gov/index.cfm%3Fadfg%3Dviewinglocations.belugapoint">Beluga Point</a> for scenic vistas. All ages. </p><p>Option to add on visit to <a href="https://www.alyeskaresort.com/">Alyeska Resort</a> for hiking/tram ride if open. </p>
 
             <p>Tuesday, July 12 - Morning: <a href="https://www.anchoragemuseum.org/">Anchorage museum </a>(with Discovery Center for kids). All ages. Afternoon: Possible <a href="https://www.muni.org/departments/parks/pages/westpool.aspx">pool with water slide</a>. All ages.</p>
 
@@ -46,6 +46,8 @@ class Index extends React.Component {
             <p><a href="https://www.humpysalaska.com/">Humpy’s</a> - Try the Man vs Food Challenge! Or maybe <a href="https://www.humpysalaska.com/humpys-great-alaskan-alehouse-2">six of us can try it together.</a> (Scroll down a bit.)</p>
             <p><a href="https://www.49statebrewing.com/">49th State Brewing</a> - Some insight from Trip Advisor. <a href="https://www.tripadvisor.com/ShowUserReviews-g60880-d10024455-r829626665-49th_State_Brewing_Anchorage-Anchorage_Alaska.html"> Give Mitch a raise!!!</a></p>
             <p><a href="https://moosestooth.net/">Moose’s Tooth</a> - "This place is hella good" from <a href="https://www.yelp.com/biz/mooses-tooth-anchorage"> Daniel O. Pearl City, HI on Yelp.</a></p>
+            <p><a href="https://rusticgoatak.com/">Rustic Goat</a> - "This place is hella good" from Clayton H. Charlotte, NC.</p>
+            <p><a href="http://www.bernieslounge.com/home.html">Bernie's Bugalow</a> - A frequent sighting spot of <a href="https://www.adn.com/voices/article/recalling-alaskas-most-notorious-drunken-moose-street-smart-buzzwinkle/2013/09/10/">Buzzwinkle, the notorious drunken moose.</a></p>
 
             </Paper>
           </Grid>
